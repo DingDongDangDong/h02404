b02458815b8aeb84027708079033401a22fd4689
Jaewook Byun <bjw0829@gmail.com>
2018-08-06 12:09:01 +0900

    Week 5 assignment1 updated

--- advanced_programming/src/main/java/kr/ac/halla/ice/advanced_programming/week5/Assignment1.java
+++ advanced_programming/src/main/java/kr/ac/halla/ice/advanced_programming/week5/Assignment1.java
@@ -4,12 +4,13 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 /**
- * Abstract callspan.txt dataset with a class
+ * Abstract citation.txt dataset with a class
  * 
- * constructor and getter and setter 
+ * constructor and getter and setter
  * 
  * callspan
  * 
@@ -24,28 +25,34 @@ public class Assignment1 {
 
 	public static void main(String[] args) {
 
+		ArrayList<Citation> citationList = new ArrayList<Citation>();
+
 		try {
-			FileReader fin = new FileReader("/home/jack/CRAWDAD/2005-07-01/callspan.txt");
+			FileReader fin = new FileReader("/home/jack/CRAWDAD/arxiv-hep-ph-coauth/trim.txt");
 
 			BufferedReader br = new BufferedReader(fin);
 
-			HashSet<String> sourceIDSet = new HashSet<>();
-			HashSet<String> destIDSet = new HashSet<>();
-			
 			while (true) {
 				String line = br.readLine();
 				if (line == null)
 					break;
-				if (line.startsWith("#") == true)
+				if (line.startsWith("%") == true)
 					continue;
-				String[] arr = line.split("\t");
+				String[] arr = line.split("\\s");
 
-				sourceIDSet.add(arr[0]);
-				destIDSet.add(arr[1]);
-				
+				String sourceID = arr[0];
+				String destinationID = arr[1];
+				long timestamp = Long.parseLong(arr[3]);
+
+				Citation citation = new Citation(sourceID, destinationID, timestamp);
+				citationList.add(citation);
 			}
-			sourceIDSet.retainAll(destIDSet);
-			System.out.println(sourceIDSet.size());
+
+			Iterator<Citation> iter = citationList.iterator();
+			while (iter.hasNext()) {
+				Citation citation = iter.next();
+				System.out.println(citation);
+			}
 			br.close();
 
 		} catch (FileNotFoundException e) {
